c1a477178e8fdb98272da50c5a67ca22b2d643c3
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-06-26 17:59:23 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int age;
+cin >> age;
+if(age>=18){
+    cout<<"You are an Adult"<<endl;
+}
+else {
+    cout<<"You are not an Adult"<<endl;
+}
+}
