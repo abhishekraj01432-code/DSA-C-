8fefa5c6c8d4abc16388dc23bf2ca10f97400655
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-08-05 10:24:16 +0530

    Add files via upload

---
+++ 15.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print15(int n){
+ for(int i=0;i<n;i++){
+    for(char ch = 'A';ch<='A'+(n-i-1);ch++){
+        cout << ch << " ";
+    }
+    cout << endl;
+ }
+ }
+int main(){
+ //for more test cases
+int t;
+cin >>t;
+for(int i=0;i<t;i++){
+     int n;
+    cin>>n;
+ print15(n);
+}
+
+}
+
+
+
+
+
+
+
